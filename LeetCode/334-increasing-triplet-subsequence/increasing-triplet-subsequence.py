3fbc50ff46b94d7e5a8cbb3e142f45af68235d40
Jayant Nehra <71202176+Jay-Nehra@users.noreply.github.com>
2024-08-21 20:15:18 +0530

    Time: 771 ms (87.30%) | Memory: 32.6 MB (65.41%) - LeetSync

---
+++ LeetCode/334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -0,0 +1,31 @@
+# """
+#     Increasing triplet subsequence
+
+
+#     nums: interger array,
+#     if there are three consusecutive elements which are in the increasing order:
+#         return true 
+
+# """
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+#         # i = 0
+#         # while i < len(nums)-2:
+#         #     if nums[i+1]>nums[i] and nums[i+2]>nums[i+1]:
+#         #         return True            
+#         #     i += 1        
+#         # return False
+        first = float('inf')
+        second = float('inf')
+
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        
+        return False
